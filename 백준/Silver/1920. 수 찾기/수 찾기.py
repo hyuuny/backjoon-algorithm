4bb9c69de1da906504ffd34d283a85a882340e6c
KIMSEONGHYUN <69466533+hyuuny@users.noreply.github.com>
2022-05-02 14:00:14 +0900

    [Silver IV] Title: 수 찾기, Time: 188 ms, Memory: 49768 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,23 +1,10 @@
 n = int(input())
-n_list = list(map(int, input().split()))
+array = set(map(int, input().split()))
 m = int(input())
-m_list = list(map(int, input().split()))
+x = list(map(int, input().split()))
 
-n_list.sort()
-
-def binary_search(value, start, end):
-  if start > end : return False
-
-  median = (start + end) // 2
-  if n_list[median] > value:
-    return binary_search(value, start, median-1)
-  elif n_list[median] < value:
-    return binary_search(value, median+1, end)
-  else:
-    return True
-
-for item in m_list:
-  if binary_search(item, 0, n-1):
-    print(1)
-  else:
-    print(0)
+for i in x:
+    if i not in array:
+        print('0')
+    else:
+        print('1')
