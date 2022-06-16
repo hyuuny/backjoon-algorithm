e90145fccba50120ba1ddfbfadf3c9d7a02d628b
KIMSEONGHYUN <shyune@knou.ac.kr>
2022-06-16 15:34:30 +0900

    [Bronze II] Title: 하얀 칸, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -0,0 +1,8 @@
+n = [list(map(str, list(input()))) for _ in range(8)]
+
+answer = 0
+for i in range(8):
+    for j in range(8):
+        if (i + j) % 2 == 0 and n[i][j] == 'F':
+            answer += 1
+print(answer)
