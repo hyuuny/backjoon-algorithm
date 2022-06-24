ccb721eaa878c97e996d768a12891d0397061e66
KIMSEONGHYUN <shyune@knou.ac.kr>
2022-06-24 10:49:06 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄷ, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for _ in range(n):
+    print('@@@@@' * n)
+
+for _ in range(n):
+    print('@' * n + '\n' + '@' * n + '\n' + '@' * n)
+
+for _ in range(n):
+    print('@@@@@' * n)
