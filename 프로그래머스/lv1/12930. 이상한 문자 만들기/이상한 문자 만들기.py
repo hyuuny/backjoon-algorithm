a5de64312ab802f830bb75e61cd3697681a94846
KIMSEONGHYUN <shyune@knou.ac.kr>
2022-07-02 22:36:07 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,21 @@
+def odd_or_even(answer, i, j):
+    if j % 2 == 0:
+        answer.append(i[j].upper())
+    else:
+        answer.append(i[j].lower())
+
+
+def solution(s):
+    words = s.lower().split(" ")
+    answer = []
+    for i in words:
+        for j in range(len(i)):
+            odd_or_even(answer, i, j)
+        answer.append(" ")
+
+    answer.pop(-1)
+    return "".join(answer)
+
+
+# 테스트를 위한 코드입니다.
+print(solution("try hello world"))
