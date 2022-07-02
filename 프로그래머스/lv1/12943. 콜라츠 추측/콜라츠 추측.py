9c14d112981b4332df67ccc157112e1826d7ff5e
KIMSEONGHYUN <shyune@knou.ac.kr>
2022-07-02 22:03:42 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,22 @@
+def solution(num):
+    cnt = 0
+    while num != 1:
+        cnt += 1
+
+        if cnt > 500:
+            return -1
+
+        if num % 2 == 0:
+            num //= 2
+            continue
+
+        if num % 2 == 1:
+            num *= 3
+            num += 1
+
+    return cnt
+
+
+print(solution(6))
+print(solution(16))
+print(solution(626331))
