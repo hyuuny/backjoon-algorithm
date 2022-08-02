aaa4e769c5baf3d56b706266e8e5944122f19d93
KIMSEONGHYUN <shyune@knou.ac.kr>
2022-08-02 15:13:59 +0900

    [Silver I] Title: 회의실 배정, Time: 24 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,28 @@
+#include "iostream"
+#include "algorithm"
+
+using namespace std;
+
+int n;
+pair<int, int> s[100005];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> s[i].second >> s[i].first;
+    }
+    // 끝나는 시간, 시작 시간 순으로 오름차순 정렬
+    sort(s, s + n);
+
+    int ans = 0;
+    int t = 0;
+    for (int i = 0; i < n; ++i) {
+        if (t > s[i].second) continue;
+        ans++;
+        t = s[i].first;
+    }
+    cout << ans;
+}
