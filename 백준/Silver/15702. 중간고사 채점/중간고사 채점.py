6ccf28a90141ca64037a4c329698e0a0397167e2
KIMSEONGHYUN <shyune@knou.ac.kr>
2022-09-04 12:23:42 +0900

    [Silver V] Title: 중간고사 채점, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15702. 중간고사 채점/중간고사 채점.py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split())
+scores = list(map(int, input().split()))
+len_score = len(scores)
+
+_dict = {}
+for i in range(m):
+    data = list(map(str, input().split()))
+    num, arr = data[0], data[1:]
+
+    _dict[num] = _dict.setdefault(num, 0)
+    for j in range(len_score):
+        if arr[j] == 'O':
+            _dict[num] += scores[j]
+
+min_student = 100001
+max_val = max(_dict.values())
+for k, v in _dict.items():
+    if v == max_val:
+        min_student = min(min_student, int(k))
+
+print(min_student, max_val)
